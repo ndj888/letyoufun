d38cad275bc5a23ba7af5cb687b8df2785e99a14
ndj888 <jjc>
2015-08-09 16:47:14 +0800

    update

--- conf/conf.js
+++ conf/conf.js
@@ -5,7 +5,7 @@
 
 var conf = {
         'common':{
-            'webHost':'http://192.168.99.190/letyoufunR010/index.php/',
+            'webHost':'http://test.letyoufun.com/',
             //'webHost':'http://192.168.99.190/letyoufunR010/index.php/',
             'notFoundImg':'http://localhost/letyoufun/statics/images/upimg/traceling1.jpg'
         }
